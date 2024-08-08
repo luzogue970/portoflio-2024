380a99b7c987d914cc0da2fca1144d9b452886ee
luzogue970 <mathieulplp970@gmail.com>
2024-08-08 10:29:09 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Nav from "./components/Nav/Nav";
+import Presentation from "./components/Presentation/Presentation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <div className="App font-montserrat">
+          <header className="App-header">
+              <Nav/>
+              <Presentation />
+          </header>
+      </div>
   );
 }
 
